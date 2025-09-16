5c92abba7cc93ce470f60cc26833c71c56b5f4d0
Timo Reymann <mail@timo-reymann.de>
2025-09-16 15:52:36 +0200

    fix: Fix parse args logic

--- cmd/main.go
+++ cmd/main.go
@@ -11,15 +11,15 @@ import (
 )
 
 func Execute() {
-	args := flag.NewFlagSet("mealie-webhook-handler", flag.ExitOnError)
-	configPath := args.String("config-file", "webhooks.toml", "Path to the config file")
-	err := args.Parse(os.Args)
+	args := flag.NewFlagSet("mealie-webhook-handler", flag.ContinueOnError)
+	var configPath string
+	args.StringVar(&configPath, "config-file", "webhooks.toml", "Path to the config file")
+	err := args.Parse(os.Args[1:])
 	if err != nil {
-		fmt.Printf("Failed to parse args: %s\n", err.Error())
 		return
 	}
 
-	configVal, err := os.ReadFile(*configPath)
+	configVal, err := os.ReadFile(configPath)
 	if err != nil {
 		fmt.Printf("Failed to open config file: %s\n", err.Error())
 		os.Exit(1)
